a6c36a6d7136914a0e816016c4cb158c1f32d5b1
Shawn Shaw <shawn.e.shaw@gmail.com>
2018-02-06 17:23:46 -0600

    console.log test

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ server.listen(80);
 
 // // app.use(bodyParser());
 // // app.use(express.methodOverride());
-app.use(express.static('/'));
+app.use('/', express.static(__dirname + '/'));
+
+console.log('someone hit the server');
 // // app.use(app.router);
 
 
