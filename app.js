e2d28fda94a49a89b64c16a6b838b6383b6538f1
Shawn Shaw <shawn.e.shaw@gmail.com>
2018-02-06 17:16:03 -0600

    testing sample

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-var express = require('express');
-var cors = require('cors');
-var server = require('http').createServer(app);
-var bodyParser = require('body-parser');
-var path = require('path');
+// var express = require('express');
+// var cors = require('cors');
+// var server = require('http').createServer(app);
+// var bodyParser = require('body-parser');
+// var path = require('path');
 
+// var app = express();
+
+// app.use(cors());
+
+// server.listen(80);   
+
+// // app.use(bodyParser());
+// // app.use(express.methodOverride());
+// app.use(express.static('shawnshaw'));
+// // app.use(app.router);
+
+
+var express = require('express');
 var app = express();
 
-app.use(cors());
+app.get('/', function(req, res){
+  res.send('hello world');
+});
 
-server.listen(80);   
-
-// app.use(bodyParser());
-// app.use(express.methodOverride());
-app.use(express.static('shawnshaw'));
-// app.use(app.router);
+app.listen(80);
