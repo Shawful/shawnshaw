650933d6b03e15837e725726ed333290cc4d5eab
Shawn Shaw <shawn.e.shaw@gmail.com>
2018-02-06 17:49:20 -0600

    try again

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ server.listen(80);
 // // app.use(bodyParser());
 // // app.use(express.methodOverride());
 // app.use('/', express.static());
-app.use(express.static(__dirname));
+app.use('/', express.static(__dirname));
 // app.use(express.static(path.join(__dirname + '/shawnshaw')));
 // app.use(express.static(path.join(__dirname + '')));
 
 
-app.get("/",function (req, res) {
-  res.sendFile(__dirname + "/index.html")
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
 // app.listen('/', function() {
