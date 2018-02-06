1a9ba03d83314a63485dfcd298309fddee0af170
Shawn Shaw <shawn.e.shaw@gmail.com>
2018-02-06 17:46:21 -0600

    looking better

--- app.js
+++ app.js
@@ -13,9 +13,14 @@ server.listen(80);
 // // app.use(bodyParser());
 // // app.use(express.methodOverride());
 // app.use('/', express.static());
-app.use(express.static(path.join(__dirname + '/')));
-app.use(express.static(path.join(__dirname + '/shawnshaw')));
-app.use(express.static(path.join(__dirname + '')));
+app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname + '/shawnshaw')));
+// app.use(express.static(path.join(__dirname + '')));
+
+
+app.get("/",function (req, res) {
+  res.sendFile(__dirname + "/index.html")
+});
 
 // app.listen('/', function() {
 // 	console.log('someone hit the server');
