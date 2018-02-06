2fc772e85902b82b66e55c9e1fc18fb0ae3f0d60
Shawn Shaw <shawn.e.shaw@gmail.com>
2018-02-06 16:36:29 -0600

    toying around

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express');
-var app = express();
 var cors = require('cors');
 var server = require('http').createServer(app);
+var bodyParser = require('body-parser');
+
+var app = express();
 
 app.use(cors());
 
 server.listen(80);   
 
-app.use(express.bodyParser());
+// app.use(bodyParser());
 app.use(express.methodOverride());
 app.use(express.static(path.join(__dirname + '~/shawnshaw')));
 app.use(app.router);
